3cf0559f110f0c959d39726b906adf438121392b
DhruvMVP <126681203+DhruvMVP@users.noreply.github.com>
2023-03-01 18:48:25 +0530

    New Project

---
+++ Flappy Bird 2D/Assets/Script/GameAssets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets instance;
+
+    public static GameAssets GetInstance()
+	{
+		return instance;
+	}
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	public Sprite pipeHeadSprite;
+
+}
---
+++ Flappy Bird 2D/Assets/Script/GameHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Started");
+
+        GameObject gameObject = new GameObject("Pipe", typeof(SpriteRenderer));
+        gameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.GetInstance().pipeHeadSprite;
+    }
+
+    
+}
